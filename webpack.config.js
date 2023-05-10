a418364d7f4654c635a197f5eca2c0f2426d1e71
Fabian Salazar <fabianxsalazar@gmail.com>
2023-05-10 16:21:38 -0700

    broken

--- webpack.config.js
+++ webpack.config.js
@@ -2,65 +2,82 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: {
-        src: '/index.js'
-    },
+    entry: './index.js'
+    ,
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
     },
     plugins: [new HtmlWebpackPlugin({
         title: 'Development',
-        template: '/index.html'
+        template: './index.html'
         }),
     ],
     module: {
         rules: [
             {
-                test: /\.jsx?/,  //this is slightly differenet in video: /.js$/
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
-            },
-            {
-                test: /\.s?css/,
-                //exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                  presets: ["@babel/preset-env", "@babel/preset-react"],
+                },
+              },
+              {
+                test: /.(css|scss)$/,
+                include: path.resolve(__dirname, "components"),
+                exclude: /node_modules/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+              },
+              {
+                test: /\.(png|jpe?g|gif)$/i, //image loader for logo
                 use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
+                  {
+                    loader: "file-loader",
+                  },
+                ],
+              },
         ]
-    },
-
-
-        ///////////////////////////////NEW STUFF AFTER COMPLETING FRONTEND
-
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'build')
     },
     devServer: {
         static: {
-          publicPath: '/build',
-          directory: path.resolve(__dirname, 'build')
+          directory: path.join(__dirname, "dist"),
         },
         compress: true,
         port: 8080,
         proxy: {
-            '/api': 'http://localhost:3000'
-        }
-
-        // proxy: {
-        //     '/api': {
-        //         target: 'http://localhost:3000',
-        //         pathRewrite: {'^/api': ''}
-        //     }
-        // }
+          "/api": {
+            target: "http://localhost:3000", //redirects requests to 3000 from 8080 if in dev
+            pathRewrite: { "^/api": "" }, //do not call localhost:3000 directly, so make all fetch reqs to /api first
+            changeOrigin: true,
+          },
+        },
+        hot: true,
+        open: true,
+        historyApiFallback: true,
+        // devtool: 'eval-source-map'
       },
+
+
+        ///////////////////////////////NEW STUFF AFTER COMPLETING FRONTEND
+
+
+    // devServer: {
+    //     static: {
+    //       publicPath: '/build',
+    //       directory: path.resolve(__dirname, 'build')
+    //     },
+    //     compress: true,
+    //     port: 8080,
+    //     proxy: {
+    //         '/api': 'http://localhost:3000'
+    //     }
+
+    //     // proxy: {
+    //     //     '/api': {
+    //     //         target: 'http://localhost:3000',
+    //     //         pathRewrite: {'^/api': ''}
+    //     //     }
+    //     // }
+    //   },
 }
