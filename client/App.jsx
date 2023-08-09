ac1b1dfe831e2824d29de8eb9c00186893ca85f0
Fabian Salazar <fabianxsalazar@gmail.com>
2023-08-08 22:00:10 -0700

    disabled ability to click more than two cards at any one time

--- client/App.jsx
+++ client/App.jsx
@@ -3,14 +3,14 @@ import SingleCard from './components/SingleCard.jsx';
 import axios from "axios"
 
 const cardImages = [
-    {"src": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png", matched: false},
-    {"src": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png", matched: false},
-    {"src": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png", matched: false},
-    {"src": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png", matched: false},
-    {"src": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/13.png", matched: false},
-    {"src": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png", matched: false},
-    // {"src": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/19.png", matched: false},
-    // {"src": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/22.png", matched: false}
+    {"src": "", matched: false},
+    {"src": "", matched: false},
+    {"src": "", matched: false},
+    {"src": "", matched: false},
+    {"src": "", matched: false},
+    {"src": "", matched: false},
+    // {"src": "", matched: false},
+    // {"src": "", matched: false}
 ]
 
 
@@ -20,9 +20,6 @@ function App(){
     const [choiceOne, setChoiceOne] = useState(null)
     const [choiceTwo, setChoiceTwo] = useState(null)
     const [disabled, setDisabled] = useState(false)
-    //const [urls, setUrls] = useState([{}])
-    //const [urls, setUrls] = useState()
-
 
     const shuffleCards = () => {
         const shuffledCards = [...cardImages, ...cardImages]
@@ -41,17 +38,18 @@ function App(){
 
     useEffect(() => {
         if(choiceTwo && choiceOne){
+            setDisabled(true)
             if(choiceOne.src === choiceTwo.src){
-                setDisabled(true)
                 setCards(prevCards => {
                     return prevCards.map(card => {
                         if(card.src === choiceOne.src){
                             return {...card, matched: true}
                         }
-                        else return card
+                        else {
+                            return card
+                        }
                     })
                 })
-                //setTimeout(() => resetTurn(), 1000)
                 resetTurn()
 
             }
@@ -73,36 +71,19 @@ function App(){
     useEffect(() => {
         const fetchUrls = async () => {
             const response = await axios.get("http://localhost:3000/api/sprites");
-            // console.log('console.log inside fetchUrls()')
-            //console.log(response.data)
             for(let i = 0; i < cardImages.length; i++){
                 cardImages[i].src = response.data[i].src;
             }
-            //console.log(cardImages)
+            shuffleCards();
         }
         fetchUrls()
-        // console.log('console.log after fetchUrls()')
-        // setTimeout(() => {
-        //     for(let i = 0; i < cardImages.length; i++){
-        //         cardImages[i].src = urls[i].src;
-        //     }
-        // }, 500)
-        setTimeout(() => shuffleCards(), 700)
     }, [])
 
-    //use this instead of the setTimeout above. This way you iliminate the delay
-    // useEffect(() => {
-    //     shuffleCards()
-    //     console.log("shuffled cards")
-    // }, [cardImages])
 
-     
     return (
         <div className='App'>
             <h1>Gotta Match 'Em All!</h1>
             <button onClick={shuffleCards}>New Game</button>
-        
-
             <div className='card-grid'>
                 {cards.map(card => (
                     <SingleCard
