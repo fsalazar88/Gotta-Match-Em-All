6baea19ce438b53ea85ab73b94adb2cecfd93ef4
Fabian Salazar <fabianxsalazar@gmail.com>
2024-05-28 14:34:53 -0700

    refactored api files

--- api/characterController.js
+++ api/characterController.js
@@ -1,33 +1,40 @@
 const characterController = {};
 
-//middleware retrieves 8 unique images from pokemon API
-  //array containing images is saved to response
+/**
+ * Middleware to retrieve 8 unique Pokémon images/sprites from the Pokémon API.
+ * The array containing image URLs is saved to the response object.
+ * 
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {Function} next - Express middleware next function
+ * 
+ * @returns {Function} Calls next middleware function
+ */
 characterController.getCharacters = async (req, res, next) => {
-    let pokemonArray = [];
+    let pokemonSprites = new Set();
     res.locals.characters = [];
 
-    while(pokemonArray.length <= 8){
-        let pokeNum = Math.floor(Math.random()*150 + 1);
-        if(pokemonArray.includes(pokeNum)) {
-          continue;
+    try {
+        while(pokemonSprites.size < 8){
+            let pokeNum = Math.floor(Math.random()*151 + 1); // Generate a random number between 1 and 151
+            if(pokemonSprites.has(pokeNum)) {
+              continue; // Skip if Pokémon number is already included
+            }
+            pokemonSprites.add(pokeNum);
+
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}/`);
+            const data = await response.json();
+    
+            // Push the Pokémon image link to the character array
+            res.locals.characters.push({src: data.sprites.front_default});
         }
-        pokemonArray.push(pokeNum);
-
-        await fetch('https://pokeapi.co/api/v2/pokemon/' + pokeNum + '/')
-          .then((data) => data.json())
-          .then((data) => {
-            let pokeImageLink = {src: data.sprites.front_default};
-            res.locals.characters.push(pokeImageLink);
-          })
-          .catch((err) => {
-            return next({
-              log: `characterController.getCharacters: ${err}`,
-              message: {error: 'error during fetch request'},
-            })
-          })
+        return next(); // Proceed to the next middleware
+    } catch (err) {
+        return next({
+            log: `characterController.getCharacters: ${err}`,
+            message: {error: 'Error during fetch request'},
+        });
     }
-    return next();
-
-}
+};
 
 module.exports = characterController;
