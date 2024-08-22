b138da504e6bc5926a1cfd054f2d4c73e742a8b8
Fabian Salazar <fabianxsalazar@gmail.com>
2024-08-21 22:58:49 -0700

    updated comments

--- index.js
+++ index.js
@@ -41,6 +41,8 @@ app.get('/', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, '/dist', 'index.html'));
 });
 
+
+
 /**
  * Global error handling middleware.
  * Catches and handles errors throughout the application.
